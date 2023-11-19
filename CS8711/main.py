2a85e1dd8e608c1c7f5dca9e1aa9e9596884da28
Harshini14037 <130223742+Harshini14037@users.noreply.github.com>
2023-11-19 11:54:35 +0530

    Update main.py

--- CS8711/main.py
+++ CS8711/main.py
@@ -1,11 +1,1 @@
-from flask import Flask, request, render_template
-import requests
-
-app = Flask(__name__)
- 
-@app.route('/', methods =["GET", "POST"])
-def index():
-  return render_template("hello world")
-    
-if __name__ == "__main__":
-    app.run()
+print("hell o world")
