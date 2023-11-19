17028baac7bb37aa77aa1f0f329a636ad376a916
Harshini14037 <130223742+Harshini14037@users.noreply.github.com>
2023-11-19 11:52:32 +0530

    Update main.py

--- CS8711/main.py
+++ CS8711/main.py
@@ -6,17 +6,6 @@ app = Flask(__name__)
 @app.route('/', methods =["GET", "POST"])
 def index():
   return render_template("hello world")
-    '''weatherData = ''
-    error = 0
-    cityName = ''
-    if request.method == "POST":      
-        cityName = request.form.get("cityname")  
-        if cityName:
-            weatherApiKey = 'Your API Key Goes Here'
-            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
-            weatherData = requests.get(url).json()
-        else:
-            error = 1    
-    return render_template('index.html', data = weatherData, cityName = cityName)'''
+    
 if __name__ == "__main__":
     app.run()
