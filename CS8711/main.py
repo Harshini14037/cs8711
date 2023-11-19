da7bc3edcde38eb7d5bc3615570d1f24c34f333c
Harshini14037 <130223742+Harshini14037@users.noreply.github.com>
2023-11-19 11:38:37 +0530

    Update main.py

--- CS8711/main.py
+++ CS8711/main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import requests
+
 app = Flask(__name__)
-@app.route('/')
-def hello():
-  """Return a friendly HTTP greeting."""
-  return 'CS8711!'
-if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8080, debug=True)
+ 
+@app.route('/', methods =["GET", "POST"])
+def index():
+  return render_template("hello world")
+    '''weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":      
+        cityName = request.form.get("cityname")  
+        if cityName:
+            weatherApiKey = 'Your API Key Goes Here'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+        else:
+            error = 1    
+    return render_template('index.html', data = weatherData, cityName = cityName)'''
+if __name__ == "__main__":
+    app.run()
