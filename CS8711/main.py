fbf127cd08fa422cf800529b9b0bde777b66ea9d
Harshini14037 <130223742+Harshini14037@users.noreply.github.com>
2023-11-19 11:21:46 +0530

    Update main.py

--- CS8711/main.py
+++ CS8711/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  """Return a friendly HTTP greeting."""
-  return 'CS8711!'
+  
+  return 'Hello world'
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
